25a707bf6bdf074f6f027e4d76a4c22f9945279b
Stefan Wanitzek <stefan@dreambyte.eu>
2016-01-10 22:58:50 +0100

    Introduce support for CoreCLR

---
+++ GoogleQR_CoreCLR/GoogleQR.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace GoogleQRGenerator
+{
+    public class GoogleQr
+    {
+        public string Data { get; set; }
+        public string Size { get; set; }
+        public bool UseHttps { get; set; }
+        public string Encoding { get; set; }
+        public string ErrorCorrection { get; set; }
+
+        public GoogleQr(string data, string size, bool useHttp)
+        {
+            Data = !string.IsNullOrEmpty(data) ? data : "http://www.google.com";
+            Size = !string.IsNullOrEmpty(size) ? size : "100x100";
+            UseHttps = useHttp;
+        }
+
+        private string BaseUrl()
+        {
+            return "http" + (UseHttps ? "s" : "") + "://chart.googleapis.com/chart?cht=qr&";
+        }
+
+        public string Render()
+        {
+            string dimensions = null;
+
+            if(!string.IsNullOrEmpty(Size))
+            {
+                var sizes = Size.Split(new[] {'x'});
+                var height = sizes[0];
+                string width = sizes[1];
+                dimensions = " height='" + height + "' width='" + width + "'";
+            }
+
+            return "<img src='" + ToString() + "'" + dimensions + " />";
+        }
+
+        public override string ToString()
+        {
+            if (!string.IsNullOrEmpty(Data))
+            {
+                var parameters = new List<string> {"chl=" + Data};
+                if (!string.IsNullOrEmpty(Size))
+                    parameters.Add("chs=" + Size);
+                if (!string.IsNullOrEmpty(Encoding))
+                    parameters.Add("choe=" + Encoding);
+                if (!string.IsNullOrEmpty(ErrorCorrection))
+                    parameters.Add("chld=" + ErrorCorrection);
+
+                return BaseUrl() + string.Join("&", parameters.ToArray());
+            }
+            throw new Exception("Data is required");
+        }
+
+
+    }
+}
---
+++ GoogleQR_CoreCLR/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GoogleQRGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleQRGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23fa4740-ee2e-40b3-ab0c-a39c73653a43")]
